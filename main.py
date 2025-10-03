1a135758f20a5111ab049e6a58035224c9fce233
kejihela <72225471+kejihela@users.noreply.github.com>
2025-10-03 11:10:49 +0100

    update main5

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from prediction_model.predict import make_prediction
 import pandas as pd
 import os
 
-'''This is like a comment, the next commment, Alright'''
+'''This is like a comment ok'''
 app = FastAPI(
 title="Loan Prediction Model API",
 description="A simple API that uses ML model to predict the Loan application status",
